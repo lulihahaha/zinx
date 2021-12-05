30d15834e29851163e8046b8bdd654322793876d
lulihahaha <1226643780@qq.com>
2021-12-05 14:00:40 +0800

    complete the base server

---
+++ znet/server.go
@@ -0,0 +1,93 @@
+package znet
+
+import (
+	"fmt"
+	"net"
+	"zinx/ziface"
+)
+
+// iServer的接口实现，定义一个Server的服务器模块
+type Server struct {
+	// 服务器的名称
+	Name string
+	// 服务器绑定的ip版本
+	IPVersion string
+	// 服务器监听的IP
+	IP string
+	// 服务器监听的端口
+	Port int
+}
+
+func (s *Server) Start() {
+	fmt.Printf("[Start] Server Listenner at IP:%s,Port %d, is starting\n", s.IP, s.Port)
+
+	go func() {
+		// 获取一个TCP的Addr
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s,%d", s.IP, s.Port))
+		if err != nil {
+			fmt.Println("resolve tcp addr error:", err)
+			return
+		}
+
+		// 监听服务器的地址
+		listenner, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listen ", s.IPVersion, "err ", err)
+		}
+
+		fmt.Println("start Zinx server successfully ", s.Name, " successfully, Listenning...")
+
+		// 阻塞等待客户端连接，处理客户端的链接业务（读写
+		for {
+			// 如果有客户端连接过来，阻塞会返回
+			conn, err := listenner.AcceptTCP()
+			if err != nil {
+				fmt.Println("Accept err", err)
+				continue
+			}
+
+			// 已经与客户端建立连接，做一些业务
+			go func() {
+				for {
+					buf := make([]byte, 512)
+					cnt, err := conn.Read(buf)
+					if err != nil {
+						fmt.Println("receive buf err", err)
+						continue
+					}
+
+					// 回显功能
+					if _, err := conn.Write(buf[:cnt]); err != nil {
+						fmt.Println("write back buf err ", err)
+						continue
+					}
+				}
+			}()
+		}
+
+	}()
+}
+
+func (s *Server) Stop() {
+	// TODO
+}
+
+func (s *Server) Serve() {
+	// 启动server的服务功能
+	s.Start()
+
+	// TODO
+
+	// 阻塞状态
+	select {}
+}
+
+func NewServer(name string) ziface.IServer {
+	s := &Server{
+		Name:      name,
+		IPVersion: "tcp4",
+		IP:        "0.0.0.0",
+		Port:      8999,
+	}
+	return s
+}
