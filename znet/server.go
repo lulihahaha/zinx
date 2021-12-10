1b01d5f6a96571b7c2cada05e8ea39990d6663d9
lulihahaha <1226643780@qq.com>
2021-12-10 12:56:04 +0800

    add error message

--- znet/server.go
+++ znet/server.go
@@ -83,7 +83,7 @@ func (s *Server) Serve() {
 	select {}
 }
 
-func NewServer(name string) ziface.IServer {
+func NewServer() ziface.IServer {
 	s := &Server{
 		Name:      utils.GlobalObject.Name,
 		IPVersion: "tcp4",
