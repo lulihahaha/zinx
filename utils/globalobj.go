af93263241237855779a8e871d2dcbe04e579ef0
lulihahaha <1226643780@qq.com>
2021-12-10 13:06:09 +0800

    add error message

--- utils/globalobj.go
+++ utils/globalobj.go
@@ -34,7 +34,7 @@ var GlobalObject *GlobalObj
 
 // 从zinx.json中去加载用于自定义的参数
 func (g *GlobalObj) Reload() {
-	data, err := ioutil.ReadFile("conf/zinx.json")
+	data, err := ioutil.ReadFile("./conf/zinx.json")
 	if err != nil {
 		fmt.Println("read file error:", err)
 		panic(err)
